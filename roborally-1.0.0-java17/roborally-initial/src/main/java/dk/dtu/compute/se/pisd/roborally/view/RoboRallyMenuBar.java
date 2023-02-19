2598af94951360c31554b089aa9176f186ae4aff
s224329 <s224329@student.dtu.dk>
2023-02-19 21:14:48 +0100

    java docs added to RoboRallyMenuBar

--- roborally-1.0.0-java17/roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/view/RoboRallyMenuBar.java
+++ roborally-1.0.0-java17/roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/view/RoboRallyMenuBar.java
@@ -48,6 +48,14 @@ public class RoboRallyMenuBar extends MenuBar {
 
     private MenuItem exitApp;
 
+    /**
+     * This class represnts the menubar "file" which contains the menu items.
+     * In other words, this is the menu bar that contains the actions that can be taken
+     * by the player, such as, new game , save game , load game , stop game  and exit.
+     * the update method is to ensure the visibility of the actions w.r.t the state of the app.
+     * @param appController
+     * the appController is one of the main controllers for the application
+     */
     public RoboRallyMenuBar(AppController appController) {
         this.appController = appController;
 
@@ -79,6 +87,10 @@ public class RoboRallyMenuBar extends MenuBar {
         update();
     }
 
+    /**
+     * This is the update method, to specify the visibilty of the menu items
+     * according to if the game is running or not.
+     */
     public void update() {
         if (appController.isGameRunning()) {
             newGame.setVisible(false);
