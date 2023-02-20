a427455d40e72a1c1166af561260d6a0cb8ed9ac
NegarOstad <s224283@student.dtu.dk>
2023-02-20 11:45:12 +0100

    Add counter and setter and getter methodes

--- roborally-1.0.0-java17/roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
+++ roborally-1.0.0-java17/roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
@@ -56,6 +56,16 @@ public class Board extends Subject {
     private int step = 0;
 
     private boolean stepMode;
+    private int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
 
     /**
      *
